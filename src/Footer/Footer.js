c8ca3c1a6d4868a4d86c126bf250636dd86980b8
kritika.dutt.upadhyay kritika.dutt.upadhyay <kdutt1@uncc.edu>
2020-10-14 21:46:16 -0400

    Brought PBudget code in

---
+++ src/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <footer id="footer">
+    <p>All rights reserved &copy; Fabio Nolasco <a href="#top">Top</a></p>
+    </footer>
+  );
+}
+
+export default Footer;
